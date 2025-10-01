827c435b897a288f45ea44d8fac59e3f0ab5ec99
Chris Gallucci <chris@gallucci.com>
2025-10-01 18:35:31 -0500

    base project console setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2024;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Advent of Code World!");
+
+        Console.WriteLine(Day01.Solution(args));
+
+        Console.Write("Press any key to continue...");
+        Console.Read();
+    }
+}
---
+++ day-01/solution.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2024;
+
+using System;
+
+class Day01
+{
+    internal static string Solution(string[] args)
+    {
+        return "Solution for Day 01";
+    }
+}
