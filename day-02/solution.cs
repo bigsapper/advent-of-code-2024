211747c320f0c22511d809c3861f13f523b9def2
Chris Gallucci <chris@gallucci.com>
2025-10-02 13:44:29 -0500

    Day 02 Part 2 solution with Aprt 1 refactor

--- day-02/solution.cs
+++ day-02/solution.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http.Headers;
 using Microsoft.VisualBasic;
@@ -15,7 +16,9 @@ public class Day02
 
         foreach (var line in lines)
         {
-            bool isSafe = Helpers.IsLineReportSafe(line);
+            List<int> reportLineValues = line.Split(" ").Select(s => Convert.ToInt32(s)).ToList();
+            //bool isSafe = Helpers.IsLineReportSafe(reportLineValues);
+            bool isSafe = Helpers.IsLineReportTolerablySafe(reportLineValues);
             if (isSafe) safeCounter++;
 
             Console.WriteLine($"Report Line: {line} is {(isSafe ? "Safe" : "Unsafe")}");
@@ -37,24 +40,21 @@ public class Day02
             return lines;
         }
 
-        internal static bool IsLineReportSafe(string reportLine)
+        internal static bool IsLineReportSafe(List<int> reportLine)
         {
             // our retval
-            bool isSafe = true; ;
+            bool isSafe = true;
 
-            var parts = reportLine.Split(" ");
-            int[] vals = parts.Select(s => Convert.ToInt32(s)).ToArray();
             // intitial validation and establish scale
-            if (vals.Length < 2) return false;
-            if (vals[0] == vals[1]) return false;
-            bool previousDirection = (vals[0] < vals[1]) ? true : false;
+            if (reportLine.Count < 2) return false;
+            if (reportLine[0] == reportLine[1]) return false;
+            bool previousDirection = (reportLine[0] < reportLine[1]) ? true : false;
 
-            // TODO: bounds checking
-            for (int i = 0; i <= vals.Length - 2; i++)
+            for (int i = 0; i <= reportLine.Count - 2; i++)
             {
                 // calculate our check values
-                bool currentDirection = (vals[i] < vals[i + 1]) ? true : false;
-                int diff = Math.Abs(vals[i] - vals[i + 1]);
+                bool currentDirection = (reportLine[i] < reportLine[i + 1]) ? true : false;
+                int diff = Math.Abs(reportLine[i] - reportLine[i + 1]);
 
                 // check if the difference is at least 1 but not more than 3
                 // check if the direction pattern is consistent
@@ -65,16 +65,23 @@ public class Day02
                 if (!isSafe) break;
             }
 
-            // if (isSafe)
-            // {
-            //     // let's remove one element at a time and see if we can get a safe result
-            //     for (int i = 0; i < vals.Length - 1; i++)   
-            //     {
-            //         var newParts = parts.Where((source, index) => index != i).ToArray();
-            //         isUnsafe = IsLineReportSafe(string.Join(" ", newParts));
-            //         if (!isUnsafe) break;
-            //     }
-            // }
+            return isSafe;
+        }
+
+        internal static bool IsLineReportTolerablySafe(List<int> reportLine)
+        {
+            if (IsLineReportSafe(reportLine)) return true;
+
+            // our retval
+            bool isSafe = false;
+
+            // let's remove one element at a time and see if we can get a safe result
+            for (int i = 0; i < reportLine.Count; i++)
+            {
+                var modifiedLine = reportLine.Where((source, index) => index != i).ToList();
+                isSafe = IsLineReportSafe(modifiedLine);
+                if (isSafe) break;
+            }
 
             return isSafe;
         }
