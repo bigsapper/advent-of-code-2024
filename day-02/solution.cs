0494e80af06c858f06532cbc647306626dc55695
Chris Gallucci <chris@gallucci.com>
2025-10-02 12:30:28 -0500

    Day 02 Part 1 solution revision

--- day-02/solution.cs
+++ day-02/solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net.Http.Headers;
 using Microsoft.VisualBasic;
 
 namespace AdventOfCode2024;
@@ -18,7 +19,7 @@ public class Day02
             if (isSafe) safeCounter++;
 
             Console.WriteLine($"Report Line: {line} is {(isSafe ? "Safe" : "Unsafe")}");
-        }   
+        }
 
         return string.Format("Solution for Day 02: {0} safe reports", safeCounter);
     }
@@ -38,31 +39,44 @@ public class Day02
 
         internal static bool IsLineReportSafe(string reportLine)
         {
-            // assume true until proven otherwise
-            bool retval = true;
+            // our retval
+            bool isSafe = true; ;
 
             var parts = reportLine.Split(" ");
             int[] vals = parts.Select(s => Convert.ToInt32(s)).ToArray();
             // intitial validation and establish scale
             if (vals.Length < 2) return false;
             if (vals[0] == vals[1]) return false;
-            bool isIncreasing = (vals[0] < vals[1]) ? true : false;
+            bool previousDirection = (vals[0] < vals[1]) ? true : false;
 
             // TODO: bounds checking
             for (int i = 0; i <= vals.Length - 2; i++)
             {
-                // check if the next value is at least 1 but not more than 3 greater than the current value
+                // calculate our check values
+                bool currentDirection = (vals[i] < vals[i + 1]) ? true : false;
                 int diff = Math.Abs(vals[i] - vals[i + 1]);
-                retval = diff >= 1 && diff <= 3;
-                if (!retval) break;
+
+                // check if the difference is at least 1 but not more than 3
                 // check if the direction pattern is consistent
-                if (vals[i] == vals[i + 1]) return false;
-                bool currentlyIsIncreasing = (vals[i] < vals[i + 1]) ? true : false;
-                retval = currentlyIsIncreasing == isIncreasing;
-                if (!retval) break;
+                isSafe = diff >= 1 && diff <= 3 && currentDirection == previousDirection;
+
+                // preserve the direction pattern only if the 2 values are not the same
+                if (diff != 0) previousDirection = currentDirection;
+                if (!isSafe) break;
             }
 
-            return retval;
+            // if (isSafe)
+            // {
+            //     // let's remove one element at a time and see if we can get a safe result
+            //     for (int i = 0; i < vals.Length - 1; i++)   
+            //     {
+            //         var newParts = parts.Where((source, index) => index != i).ToArray();
+            //         isUnsafe = IsLineReportSafe(string.Join(" ", newParts));
+            //         if (!isUnsafe) break;
+            //     }
+            // }
+
+            return isSafe;
         }
     }
 }
